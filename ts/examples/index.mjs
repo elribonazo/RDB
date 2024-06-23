bef263115c187d0dea0f3a38e0d0a0410d7a024f
Javier Ribó <elribonazo@gmail.com>
2024-06-23 14:31:33 +0200

    fix: release fix

--- ts/examples/index.mjs
+++ ts/examples/index.mjs
@@ -1,3 +1,26 @@
-import ridb from '../build/esm/index.js';
-console.log(ridb.BaseStorage)
-debugger;
+import {
+    RIDB,
+    SchemaFieldType
+} from '../build/esm/index.mjs';
+
+(async () => {
+    const db =  new RIDB(
+        {
+            demo: {
+                version: 0,
+                primaryKey: 'id',
+                type:SchemaFieldType.object,
+                properties: {
+                    id: {
+                        type:SchemaFieldType.string,
+                        maxLength: 60
+                    }
+                }
+            }
+
+        }
+    )
+    console.log("Starting the database")
+    await db.start()
+    console.log("Ok :)")
+})()
