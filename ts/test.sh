7dfa116dabd37b930957b52ebc3022e6c62470c6
Javier Ribó <elribonazo@gmail.com>
2024-06-23 16:06:00 +0200

    docs: improve

--- ts/test.sh
+++ ts/test.sh
@@ -24,10 +24,10 @@ BROWSER_ENV="--environment jsdom --browser --browser.name=chrome --run"
 # Check which environment to test based on the options provided
 if [ "$ENVIRONMENT" = "node" ]; then
     echo "Testing ESM Version on Node"
-    npx vitest --config "vitest.config.ts" $NODE_ENV tests/node.test.ts
+    VITE_CJS_IGNORE_WARNING=true npx vitest --config "vitest.config.ts" $NODE_ENV tests/node.test.ts
 elif [ "$ENVIRONMENT" = "browser" ]; then
     echo "Testing ESM Version in Browser"
-    npx vitest --config "vitest.config.ts" $BROWSER_ENV tests/browser.test.ts
+    VITE_CJS_IGNORE_WARNING=true npx vitest --config "vitest.config.ts" $BROWSER_ENV tests/browser.test.ts
 else
     echo "Error: Unknown environment specified. Please use '-e node' or '-e browser'."
     exit 1
